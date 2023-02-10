239d0b8ed5753a59709cb0cb6d93af2b4c707bd3
AjisaiHachimitsu <AjisaiHachimitsu@users.noreply.github.com>
2023-02-10 07:42:04 +0000

    deploy: 8c0cc9efdb0ab903fa245a7d179b29c219a04ab1

---
+++ app/immutable/chunks/2-036518b5.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-fe18496f.js";export{t as component};
--- app/immutable/chunks/2-66c103e2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-837ab1a1.js";export{t as component};
